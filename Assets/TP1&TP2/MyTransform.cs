0b5b94ece23afdb19ad18c0858f44490d0a0e13d
MateoMonastra <mateomonastravm@gmail.com>
2024-11-28 18:26:26 -0300

    Add Transform base

--- Assets/TP1&TP2/MyTransform.cs
+++ Assets/TP1&TP2/MyTransform.cs
@@ -1,18 +1,245 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using TP1_TP2.Utilities;
 using UnityEngine;
 
-public class MyTransform : MonoBehaviour
+namespace TP1_TP2
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MyTransform : MonoBehaviour
     {
-        
-    }
+        public MyTransform()
+        {
+            throw new NotImplementedException();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void DetachChildrens()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DetachChildren(MyTransform children)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetChildCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParent(MyTransform newParent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParent(MyTransform parent, bool worldPositionStays)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddChildren(MyTransform children)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LookAt(Vec3 targetPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LookAt(Vec3 targetPosition, Vec3 worldUp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LookAt(MyTransform target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LookAt(Transform target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LookAt(MyTransform target, Vec3 worldUp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LookAt(Transform target, Vec3 worldUp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(Vec3 eulers, Space relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(Vec3 eulers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(float xAngle, float yAngle, float zAngle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(float xAngle, float yAngle, float zAngle, Space relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(Vec3 axis, float angle, Space relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(Vec3 axis, float angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RotateAround(Vec3 point, Vec3 axis, float angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RotateAround(Vec3 axis, float angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLocalPositionAndRotation(Vec3 localPosition, MyQuaternion rotation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPositionAndRotation(Vec3 position, MyQuaternion rotation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translate(Vec3 translation, Space relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translate(Vec3 translation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translate(float x, float y, float z, Space relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translate(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translate(Vec3 translation, MyTransform relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translate(float x, float y, float z, MyTransform relativeTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scale(Vec3 scale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLocalScale(Vec3 newScale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 TransformDirection(Vec3 direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 TransformDirection(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 TransformPoint(Vec3 position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 TransformPoint(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 TransformVector(Vec3 vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 TransformVector(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 InverseTransformDirection(Vec3 direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 InverseTransformDirection(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 InverseTransformPoint(Vec3 position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 InverseTransformPoint(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 InverseTransformVector(Vec3 vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vec3 InverseTransformVector(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateLocalToWorldMatrix()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateWorldToLocalMatrix()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateMatrix()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateChildrens()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
